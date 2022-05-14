dd116f898702249e01920530ceec1251ba304313
Zaida Hammel <99354763+zaida-hammel@users.noreply.github.com>
2022-05-14 11:40:24 -0400

    edited code

--- js/script.js
+++ js/script.js
@@ -1,8 +1,36 @@
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Zaida Hammel
+// Created on: May 2020
 // This file contains the JS functions for index.html
 
+/**
+ * Check servie worker.
+ */
+ if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Unit5-08-JS/sw.js", {
+    scope: "/ICS2O-Unit5-08-JS/",
+  })
+}
+
+;("use strict")
+/**
+ * This function calculates area of a parrallelogram.
+ */
+/**
+ * Input
+ */
 function myButtonClicked() {
-  // this function displays "Hello, World!"
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>";
+  const counter1 = parseInt(document.getElementById("counter1").value);
+  const counter2 = parseInt(document.getElementById("counter2").value);
+  var addedNumber = 0;
+  var answer = counter1;
+
+  if (counter1 > 0 && counter2 > 0) {
+    while (answer >= counter2) {
+      addedNumber = addedNumber + 1;
+      answer = answer - counter2;
+    }
+  }
+
+  document.getElementById("answers").innerHTML = 
+      counter1 + " ÷ " + counter2 + " = " + addedNumber;
 }
